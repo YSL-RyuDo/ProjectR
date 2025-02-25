a164d3ccf8180afc6da2d18cb434751736bd8028
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2025-02-26 01:50:56 +0900

    플레이어 기본 이동 및 점프 구현, 벽타기 구현 중

--- Assets/Scripts/Core/Unit/Player/PlayerController.cs
+++ Assets/Scripts/Core/Unit/Player/PlayerController.cs
@@ -2,17 +2,104 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//InputManage 사용을 위한 네임스페이스
+using Manager.InputManager;
 
-    // Update is called once per frame
-    void Update()
+namespace Core.Unit.Player
+{
+    public class PlayerController : MonoBehaviour
     {
-        
+        private Rigidbody rigid;
+
+        public float moveSpeed = 5f;
+        public float jumpForce = 15f; // 게임 프로젝트 세팅에서 중력 -90으로 설정함, 유니티 엔진에서 25로 설정
+        public float wallMoveSpeed = 3f;
+
+        [SerializeField]
+        private bool isGrounded;
+        private bool isWallAttached; //벽에 붙었는지
+        private Vector3 wallNormal; //벽의 방향
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();  
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleMovement();
+        }
+
+        void HandleMovement()
+        {
+            float moveX = InputManager.instance.horizontal; // 좌우 입력
+            float moveZ = InputManager.instance.vertical;   // 앞뒤 입력 (벽에선 위아래로 변환)
+            bool jump = InputManager.instance.jump;
+
+            // 벽에 붙어있는 경우
+            if (isWallAttached)
+            {
+                rigid.useGravity = false; // 중력 제거
+
+                //입력 변환
+                Vector3 moveDirection = new Vector3(moveX, moveZ, 0).normalized * wallMoveSpeed;
+
+                // 벽의 방향을 따라 이동 방향 조정
+                moveDirection = Vector3.ProjectOnPlane(new Vector3(moveX, moveZ, 0), wallNormal);
+
+                rigid.velocity = moveDirection;
+
+                // 벽을 타고 올라가다가 바닥에 닿으면 원래 상태로 복귀
+                if (isGrounded)
+                {
+                    isWallAttached = false;
+                    rigid.useGravity = true;
+                }
+
+                return; // 벽에 붙어있을 때는 기존 이동 로직 실행 안 함
+            }
+
+            // 일반 이동 로직
+            Vector3 normalMove = new Vector3(moveX, 0, moveZ).normalized * moveSpeed;
+            rigid.velocity = new Vector3(normalMove.x, rigid.velocity.y, normalMove.z);
+
+            if (jump && isGrounded)
+            {
+                rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
+            }
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.CompareTag("Ground"))
+            {
+                Vector3 normal = collision.contacts[0].normal;
+
+                // 바닥인지 확인 (y 값이 충분히 크면 바닥)
+                if (normal.y > 0.5f)
+                {
+                    isGrounded = true;
+                    isWallAttached = false; // 벽에서 내려오면 벽 붙기 상태 해제
+                    rigid.useGravity = true; // 중력 다시 활성화
+                }
+                else if (!isGrounded && Mathf.Abs(normal.y) < 0.3f)
+                {
+                    // 바닥이 아닌 경우 (벽이면)
+                    isWallAttached = true;
+                    wallNormal = normal; // 벽 방향 저장
+                    rigid.velocity = Vector3.zero; // 속도 초기화
+                    rigid.useGravity = false; // 중력 제거 (벽에 붙음)
+                }
+            }
+            
+
+        }
     }
 }
+
+
+
--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+//입력 매니저, 입력을 감지하고 저장함
+namespace Manager.InputManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InputManager : MonoBehaviour
     {
-        
-    }
+        //싱글톤 패턴 적용, 읽기 전용 프로퍼티로 설정
+        //싱글톤 패턴으로 다른 스크립트에서 네임스페이스 설정 후 InputManager.instance로 접근 가능
+        public static InputManager instance { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //수평 이동 정보
+        public float horizontal {  get; private set; }
+
+        //수직 이동 정보
+        public float vertical { get; private set; } 
+
+        //점프
+        public bool jump {  get; private set; }
+
+        private void Awake()
+        {
+            //인스턴스로 처음 실행될 때 자기 자신만 존재하도록 함
+            if (instance == null) instance = this;
+            else Destroy(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            jump = Input.GetKeyDown(KeyCode.LeftAlt);
+        }
     }
 }
+
